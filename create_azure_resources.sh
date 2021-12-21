279846dd35a5823678fa8b1c38cb206151fd82c8
Shivashant <81952290+Shivashant25@users.noreply.github.com>
2021-12-22 00:01:15 +0530

    Update create_azure_resources.sh

--- create_azure_resources.sh
+++ create_azure_resources.sh
@@ -7,6 +7,8 @@ function replace_json_field {
     rm "$tmpfile"
 }
 
+AZURE_CREDENTIALS={ "clientId": "943abbae-8858-4aae-b132-93efa094dd72", "clientSecret": "Spg32RMDOi66yQSyOIet4Bcrmv6Xx.1dT1", "subscriptionId": "a5932f36-63c8-4bc7-b97a-f90a2900b8a1", "tenantId": "495d5cd4-4a6c-4d4e-bdaa-5e1f96a097aa", "activeDirectoryEndpointUrl": "https://login.microsoftonline.com", "resourceManagerEndpointUrl": "https://management.azure.com/", "activeDirectoryGraphResourceId": "https://graph.windows.net/", "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/", "galleryEndpointUrl": "https://gallery.azure.com/", "managementEndpointUrl": "https://management.core.windows.net/" }
+
 # Check if SUFFIX envvar exists
 if [[ -z "${MCW_SUFFIX}" ]]; then
     echo "Please set the MCW_SUFFIX environment variable to a unique three character string."
@@ -49,7 +51,6 @@ sed -i "s/\[SUFFIX\]/$MCW_SUFFIX/g" ~/Fabmedical/.github/workflows/content-web.y
 ACR_CREDENTIALS=$(az acr credential show -n fabmedical$MCW_SUFFIX)
 ACR_USERNAME=$(jq -r -n '$input.username' --argjson input "$ACR_CREDENTIALS")
 ACR_PASSWORD=$(jq -r -n '$input.passwords[0].value' --argjson input "$ACR_CREDENTIALS")
-AZURE_CREDENTIALS=$(az ad sp create-for-rbac --sdk-auth)
 
 GITHUB_TOKEN=$MCW_GITHUB_TOKEN
 cd ~/Fabmedical
