e96221bee6caeb875800e9c53c3909811fe8e114
Shivashant <81952290+Shivashant25@users.noreply.github.com>
2022-01-04 06:47:02 +0000

    Update azure_create_resources_02.sh

--- azure_create_resources_02.sh
+++ azure_create_resources_02.sh
@@ -7,7 +7,7 @@ function replace_json_field {
     rm "$tmpfile"
 }
 
-AZURE_CREDENTIALS={ "clientId": "1c9046b8-1de8-423d-8c4e-ac3d2307e2c5", "clientSecret": "ebye41PTL*CT", "subscriptionId": "04b1e231-3aa7-49b7-9ff2-41a9eef7a1da", "tenantId": "338cad0a-9226-41c6-a09d-748ec1a0b210", "activeDirectoryEndpointUrl": "https://login.microsoftonline.com", "resourceManagerEndpointUrl": "https://management.azure.com/", "activeDirectoryGraphResourceId": "https://graph.windows.net/", "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/", "galleryEndpointUrl": "https://gallery.azure.com/", "managementEndpointUrl": "https://management.core.windows.net/" }
+AZURE_CREDENTIALS="{ "clientId": "1c9046b8-1de8-423d-8c4e-ac3d2307e2c5", "clientSecret": "ebye41PTL*CT", "subscriptionId": "04b1e231-3aa7-49b7-9ff2-41a9eef7a1da", "tenantId": "338cad0a-9226-41c6-a09d-748ec1a0b210", "activeDirectoryEndpointUrl": "https://login.microsoftonline.com", "resourceManagerEndpointUrl": "https://management.azure.com/", "activeDirectoryGraphResourceId": "https://graph.windows.net/", "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/", "galleryEndpointUrl": "https://gallery.azure.com/", "managementEndpointUrl": "https://management.core.windows.net/" }"
 
 # Check if SUFFIX envvar exists
 if [[ -z "${MCW_SUFFIX}" ]]; then
