89bbad83bff04c75da9dda84455dfce26d3cd15d
Shivashant <81952290+shivashant25@users.noreply.github.com>
2022-05-13 14:21:11 +0530

    Update logontask.ps1

--- continous-delivery/logontask.ps1
+++ continous-delivery/logontask.ps1
@@ -99,29 +99,44 @@ $path = "C:\Workspaces\lab\mcw-continuous-delivery-lab-files\infrastructure"
 
 sleep 20
 
-#Import Common Functions
-$commonscriptpath = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.12\Downloads\0\cloudlabs-common\cloudlabs-windows-functions.ps1"
-. $commonscriptpath
-
 #check status of docker app installation aand cloned lab files
 $app = Get-Item -Path 'C:\Program Files\Docker\Docker\Docker Desktop.exe' 
 $clonefiles = Get-Item -Path 'C:\Workspaces\lab\mcw-continuous-delivery-lab-files\content-api'
 
 if(($app -ne $null) -and ($clonefiles -ne $null))
 {
-    Write-Output "succeeded"
-    $Validstatus = 'succeeded'
+    Write-Information "Validation Passed"
+    
+    $validstatus = "Successfull
 }
 else {
     Write-Warning "Validation Failed - see log output"
-    $Validstatus="Failed"  ##Failed or Successful at the last step
-    $Validmessage="PS script execution failed"
+    $validstatus = "Failed
       }
-
-sleep 3
-#Set the final deployment status
-CloudlabsManualAgent setStatus
       
+Function SetDeploymentStatus($ManualStepStatus, $ManualStepMessage)
+{
+
+    (Get-Content -Path "C:\WindowsAzure\Logs\status-sample.txt") | ForEach-Object {$_ -Replace "ReplaceStatus", "$ManualStepStatus"} | Set-Content -Path "C:\WindowsAzure\Logs\validationstatus.txt"
+    (Get-Content -Path "C:\WindowsAzure\Logs\validationstatus.txt") | ForEach-Object {$_ -Replace "ReplaceMessage", "$ManualStepMessage"} | Set-Content -Path "C:\WindowsAzure\Logs\validationstatus.txt"
+}
+ if ($validstatus -eq "Successfull") {
+    $ValidStatus="Succeeded"
+    $ValidMessage="Environment is validated and the deployment is successful"
+    
+Remove-Item 'C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt' -force
+      }
+else {
+    Write-Warning "Validation Failed - see log output"
+    $ValidStatus="Failed"
+    $ValidMessage="Environment Validation Failed and the deployment is Failed"
+      } 
+ SetDeploymentStatus $ValidStatus $ValidMessage
+
+#Import Common Functions
+$commonscriptpath = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.12\Downloads\0\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
 sleep 3
 #Start the cloudlabs agent service 
 CloudlabsManualAgent Start
