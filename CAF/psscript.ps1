491e3ac126dbcfda4d3cb24374aead3f1a6c382e
Shivashant <81952290+shivashant25@users.noreply.github.com>
2022-10-13 07:42:18 +0530

    Create psscript.ps1

---
+++ CAF/psscript.ps1
@@ -0,0 +1,114 @@
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
+
+    [string]
+    $AzurePassword,
+
+    [string]
+    $AzureTenantID,
+
+    [string]
+    $AzureSubscriptionID,
+
+    [string]
+    $ODLID,
+
+    [string]
+    $DeploymentID,
+
+    [string]
+    $OBJECTID,
+
+    [string]
+    $InstallCloudLabsShadow,
+
+    [string]
+    $vmAdminUsername,
+
+    [string]
+    $trainerUserName,
+
+    [string]
+    $trainerUserPassword,
+
+    [string]
+    $adminPassword
+)
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+#InstallManualStatusAgent
+CloudlabsManualAgent Install
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+WindowsServerCommon
+InstallAzPowerShellModule
+#InstallAzCLI
+#InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
+Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID $OBJECTID
+
+$FileDir ="C:\LabFiles"
+
+
+#Install az cli
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://azcliprod.blob.core.windows.net/msi/azure-cli-2.21.0.msi","C:\Packages\azure-cli-2.21.0.msi")
+sleep 2
+Start-Process msiexec.exe -Wait '/I C:\Packages\azure-cli-2.21.0.msi /qn' -Verbose
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/cloud-adoption-framework/files/vnetdmz-deploy.json","C:\LabFiles\vnetdmz-deploy.json")
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/cloud-adoption-framework/scripts/logontask.ps1","C:\LabFiles\logontask.ps1")
+
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/cloud-adoption-framework/scripts/logontask1.ps1","C:\LabFiles\logontask1.ps1")
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/cloud-adoption-framework/files/cleanup.ps1","C:\LabFiles\cleanup.ps1")
+
+
+
+#Enable Autologon
+$AutoLogonRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultUsername" -Value "$($env:ComputerName)\demouser" -type String  
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultPassword" -Value "$adminPassword" -type String
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoLogonCount" -Value "1" -type DWord
+
+# Scheduled Task
+$Trigger= New-ScheduledTaskTrigger -AtLogOn
+$User= "$($env:ComputerName)\demouser" 
+$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File $FileDir\logontask.ps1"
+Register-ScheduledTask -TaskName "Setup" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force 
+
+
+#Connect-AzureAD -Credential $cred | Out-Null
+
+#az login --username "$userName" --password "$password"
+
+
+#New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $OBJECTID
+
+#sleep 300
+
+#az role assignment create  --scope '/' --role 'owner' --assignee $AzureUserName 
+
+#sleep 300
+
+CloudlabsManualAgent Start
+
+Restart-Computer -Force
