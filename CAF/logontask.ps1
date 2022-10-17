be3856ad948e9a018d5df5dfa92d584a4c1cf656
Shivashant <81952290+shivashant25@users.noreply.github.com>
2022-10-17 15:43:25 +0530

    Update logontask.ps1

--- CAF/logontask.ps1
+++ CAF/logontask.ps1
@@ -72,7 +72,7 @@ Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoLogonCount" -Value "1" -type
 # Scheduled Task
 $Trigger= New-ScheduledTaskTrigger -AtLogOn
 $User= "$($env:ComputerName)\demouser" 
-$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File $FileDir\logontask1.ps1"
+$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File $FileDir\deploybicep.ps1"
 Register-ScheduledTask -TaskName "Setup1" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force 
 
 
