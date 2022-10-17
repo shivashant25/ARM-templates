910988f03d613251fe2289ce5b416faf0dc8fae4
Shivashant <81952290+shivashant25@users.noreply.github.com>
2022-10-17 15:26:16 +0530

    Create logontask.ps1

---
+++ CAF/logontask.ps1
@@ -0,0 +1,328 @@
+Set-ExecutionPolicy -ExecutionPolicy bypass -Force
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsLogOnTask.txt -Append
+
+. C:\LabFiles\AzureCreds.ps1
+
+$userName = $AzureUserName
+$password = $AzurePassword
+
+az login --username "$userName" --password "$password"
+
+
+refreshenv
+sleep 2
+az rest --method post --url "/providers/Microsoft.Authorization/elevateAccess?api-version=2016-07-01"
+
+sleep 10
+az logout
+
+sleep 5
+az login --username "$userName" --password "$password"
+
+refreshenv
+az role assignment create  --scope '/' --role 'owner' --assignee $userName 
+
+
+
+function validateroleassignment{
+                       
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+Connect-AzAccount -Credential $cred | Out-Null
+$roles = Get-AzRoleAssignment -SignInName  $userName    -Scope '/'
+$roles1 = $roles[1]
+$rolename = $roles1.RoleDefinitionName
+$rolescope = $roles1.Scope
+
+if( "$rolename -eq 'Owner' -and $rolescope -eq '/' " -or "$rolename2 -eq 'Owner' -and $rolescope2 -eq "/" "-or "$rolename3 -eq 'Owner' -and $rolescope3 -eq "/" ")
+
+{
+Write-Output "Owner role is already available"
+
+$break
+
+}
+else{
+Write-Output "adding the role assignment"
+az role assignment create  --scope '/' --role 'owner' --assignee $userName 
+
+Start-Sleep 50
+
+Write-Output "refreshing the azure creds"
+
+Disconnect-AzAccount
+Write-Output "calling the function again to get the latest role assignments"
+
+ validateroleassignment
+
+}
+
+}
+validateroleassignment
+
+Disconnect-AzAccount
+
+#Enable Autologon
+$AutoLogonRegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultUsername" -Value "$($env:ComputerName)\demouser" -type String  
+Set-ItemProperty -Path $AutoLogonRegPath -Name "DefaultPassword" -Value "Password.1!!" -type String
+Set-ItemProperty -Path $AutoLogonRegPath -Name "AutoLogonCount" -Value "1" -type DWord
+
+# Scheduled Task
+$Trigger= New-ScheduledTaskTrigger -AtLogOn
+$User= "$($env:ComputerName)\demouser" 
+$Action= New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe" -Argument "-executionPolicy Unrestricted -File $FileDir\logontask1.ps1"
+Register-ScheduledTask -TaskName "Setup1" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest -Force 
+
+
+Disconnect-AzAccount
+
+#sleep 600
+
+#. C:\LabFiles\AzureCreds.ps1
+
+#$userName = $AzureUserName
+#$password = $AzurePassword
+
+#az login --username "$userName" --password "$password"
+
+#az role assignment create  --scope '/' --role 'owner' --assignee $AzureUserName 
+
+Set-ExecutionPolicy -ExecutionPolicy bypass -Force
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsLogOnTask.txt -Append
+
+cd C:\BicepTemplates
+
+git clone --branch main https://github.com/shivashant25/eslz-bicep.git
+
+
+. C:\LabFiles\AzureCreds.ps1
+
+$userName = $AzureUserName
+$password = $AzurePassword
+$subscriptionId = $AzureSubscriptionID
+$TenantID = $AzureTenantID
+
+
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+Connect-AzAccount -Credential $cred | Out-Null
+
+
+cd eslz-bicep
+
+refreshenv
+
+#BICEP DEPLOYMENTS STARTS FROM HERE
+
+#Deploy Management groups
+$mgmttemplate = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\managementGroups"
+$mgmtparameters = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\managementGroups\parameters"
+
+New-AzTenantDeployment -TemplateFile "$mgmttemplate\managementGroups.bicep" -TemplateParameterFile "$mgmtparameters\managementGroups.parameters.all.json" -Location "centralus" 
+
+
+
+# Deploy Azure policy definitions
+$cpdtemplate = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\policy\definitions"
+$cpdparameters = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\policy\definitions\parameters"
+
+New-AzManagementGroupDeployment `
+  -TemplateFile "$cpdtemplate\customPolicyDefinitions.bicep" `
+  -TemplateParameterFile "$cpdparameters\customPolicyDefinitions.parameters.all.json" `
+  -Location "centralus" `
+  -ManagementGroupId eslz
+
+
+
+# Deploy custom roles
+$rbactemplate = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\customRoleDefinitions"
+$rbacparameters = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\customRoleDefinitions\parameters"
+
+New-AzManagementGroupDeployment `
+  -TemplateFile "$rbactemplate\customRoleDefinitions.bicep" `
+  -TemplateParameterFile "$rbacparameters\customRoleDefinitions.parameters.all.json" `
+  -Location centralus `
+  -ManagementGroupId eslz  
+
+
+
+
+# Deploy Log Analytics
+
+# Set Management Sub - suffix subscription  ID as the the current subscription
+
+$AllsubID = (Get-AzSubscription).Id
+
+$ManagementSubscriptionId = $AllsubID[6]
+
+Select-AzSubscription -SubscriptionId $ManagementSubscriptionId
+
+# Create Resource Group - optional when using an existing resource group
+New-AzResourceGroup `
+  -Name eslz-mgmt `
+  -Location centralus
+
+$logtemplate = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\logging"
+$logparameters = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\logging\parameters"
+
+New-AzResourceGroupDeployment `
+  -TemplateFile "$logtemplate\logging.bicep" `
+  -TemplateParameterFile "$logparameters\logging.parameters.all.json" `
+  -ResourceGroup eslz-mgmt
+
+
+
+# Deploy Hub Networking before deploying orchestration
+# Set Platform connectivity subscription ID which is L1 - Connectivity Sub - Sufiix
+
+$AllsubID = (Get-AzSubscription).Id
+
+$ConnectivitySubscriptionId = $AllsubID[1]
+
+Select-AzSubscription -SubscriptionId $ConnectivitySubscriptionId
+
+New-AzResourceGroup -Name 'eslz_Hub_Networking' `
+  -Location 'centralus'
+
+$nettemplate = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\hubNetworking"
+$netparameters = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\/hubNetworking\parameters"
+
+  
+New-AzResourceGroupDeployment `
+  -TemplateFile "$nettemplate\hubNetworking.bicep" `
+  -TemplateParameterFile "$netparameters\hubNetworking.parameters.all.json" `
+  -ResourceGroupName 'eslz_Hub_Networking'
+
+
+#Replacing connectivity Sub ID and Hub Vnet ResourceId in parameters file
+
+$AllsubID = (Get-AzSubscription).Id
+
+$ConnectivitySubscriptionId = $AllsubID[1]
+
+Select-AzSubscription -SubscriptionId $ConnectivitySubscriptionId
+
+
+(Get-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\hubPeeredSpoke\parameters\hubPeeredSpoke.parameters.all.json") | ForEach-Object {$_ -Replace "connectivitysubid", $ConnectivitySubscriptionId} | Set-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\hubPeeredSpoke\parameters\hubPeeredSpoke.parameters.all.json"
+
+$hubvnet = (Get-AzResource -Name "eslz-hub-centralus").ResourceId
+
+(Get-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\hubPeeredSpoke\parameters\hubPeeredSpoke.parameters.all.json") | ForEach-Object {$_ -Replace "hubvnetid", $hubvnet} | Set-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\hubPeeredSpoke\parameters\hubPeeredSpoke.parameters.all.json"
+
+  # Deploy hubPeeredSpoke - Spoke network
+
+$hpstemplate = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\hubPeeredSpoke"
+$hpsparameters = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\hubPeeredSpoke\parameters"
+
+New-AzManagementGroupDeployment `
+  -TemplateFile "$hpstemplate\hubPeeredSpoke.bicep" `
+  -TemplateParameterFile "$hpsparameters\hubPeeredSpoke.parameters.all.json" `
+  -Location centralus `
+  -ManagementGroupId eslz
+
+
+
+#create service pricipal and get oject ID
+
+$servicePrincipalDisplayName = "eslzsp"
+$servicePrincipal = New-AzADServicePrincipal -DisplayName $servicePrincipalDisplayName
+$sp = (Get-AzADServicePrincipal -DisplayName 'eslzsp').Id
+(Get-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\roleAssignments\parameters\roleAssignmentManagementGroup.servicePrincipal.parameters.all.json") | ForEach-Object {$_ -Replace "spid", $sp} | Set-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\roleAssignments\parameters\roleAssignmentManagementGroup.servicePrincipal.parameters.all.json"
+
+#Replacing SP object ID in parameters
+
+# Assign role 
+
+$ratemplate = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\roleAssignments"
+$raparameters = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\roleAssignments\parameters"
+
+New-AzManagementGroupDeployment `
+  -TemplateFile "$ratemplate\roleAssignmentManagementGroup.bicep" `
+  -TemplateParameterFile "$raparameters\roleAssignmentManagementGroup.servicePrincipal.parameters.all.json" `
+  -ManagementGroupId eslz-platform `
+  -Location centralus
+
+
+
+
+#Subscription placement
+
+$AllsubID = (Get-AzSubscription).Id
+
+$ConnectivitySubscriptionId = $AllsubID[1]
+$MigrationLZSubscriptionId = $AllsubID[2]
+$GovernanceSubscriptionId = $AllsubID[3]
+$IdentitySubscriptionId = $AllsubID[4]
+$IdentitySubscriptionId = $AllsubID[4]
+$LandingZoneSubscriptionId= $AllsubID[5]
+$ManagementSubscriptionId = $AllsubID[6]
+
+
+(Get-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\subPlacementAll\parameters\subPlacementAll.parameters.all.json") | ForEach-Object {$_ -Replace "managementsubid", $ManagementSubscriptionId} | Set-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\subPlacementAll\parameters\subPlacementAll.parameters.all.json"
+(Get-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\subPlacementAll\parameters\subPlacementAll.parameters.all.json") | ForEach-Object {$_ -Replace "identitysubid", $IdentitySubscriptionId} | Set-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\subPlacementAll\parameters\subPlacementAll.parameters.all.json"
+(Get-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\subPlacementAll\parameters\subPlacementAll.parameters.all.json") | ForEach-Object {$_ -Replace "corpsubid", $LandingZoneSubscriptionId} | Set-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\subPlacementAll\parameters\subPlacementAll.parameters.all.json"
+
+
+$subparameters= "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\subPlacementAll\parameters"
+$subtemplate = "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\subPlacementAll"
+
+# For Azure global regions
+New-AzManagementGroupDeployment `
+  -TemplateFile "$subtemplate\subPlacementAll.bicep" `
+  -TemplateParameterFile "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\orchestration\subPlacementAll\parameters\subPlacementAll.parameters.all.json" `
+  -Location centralus `
+  -ManagementGroupId eslz
+
+
+#Replacing ResourceId
+$ddosid= (Get-AzResource -Name "eslz-ddos-plan").ResourceId
+(Get-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\policy\assignments\alzDefaults\parameters\alzDefaultPolicyAssignments.parameters.all.json") | ForEach-Object {$_ -Replace "ddosid", $ddosid} | Set-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\policy\assignments\alzDefaults\parameters\alzDefaultPolicyAssignments.parameters.all.json"
+
+
+$AllsubID = (Get-AzSubscription).Id
+
+$ManagementSubscriptionId = $AllsubID[6]
+
+
+Select-AzSubscription -SubscriptionId $ManagementSubscriptionId
+
+$loganalyticsid= (Get-AzResource -ResourceGroupName "eslz-mgmt" -ResourceType "Microsoft.OperationalInsights/workspaces" ).ResourceId
+
+(Get-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\policy\assignments\alzDefaults\parameters\alzDefaultPolicyAssignments.parameters.all.json") | ForEach-Object {$_ -Replace "logid", $loganalyticsid} | Set-Content -Path "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\policy\assignments\alzDefaults\parameters\alzDefaultPolicyAssignments.parameters.all.json"
+
+# For Azure global regions
+New-AzManagementGroupDeployment `
+  -TemplateFile "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\policy\assignments\alzDefaults\alzDefaultPolicyAssignments.bicep" `
+  -TemplateParameterFile "C:\BicepTemplates\eslz-bicep\infra-as-code\bicep\modules\policy\assignments\alzDefaults\parameters\alzDefaultPolicyAssignments.parameters.all.json" `
+  -Location centralus `
+  -ManagementGroupId eslz
+
+#Import Common Functions
+#$commonscriptpath = "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.12\Downloads\0\cloudlabs-common\cloudlabs-windows-functions.ps1"
+#. $commonscriptpath
+
+#$status = (Get-AzRoleAssignment -Scope '/' -RoleDefinitionName "Owner"-SignInName $AzureUserName)
+#$status
+#if ($status -ne $null)
+#{
+ 
+#    $Validstatus="Succeeded"  ##Failed or Successful at the last step
+#   $Validmessage="Role assignment succeeded"
+
+
+
+#}
+#else {
+#    Write-Warning "Validation Failed - see log output"
+ #   $Validstatus="Failed"  ##Failed or Successful at the last step
+#    $Validmessage="Owner role assignment for object id failed"
+      }
+
+
+#CloudlabsManualAgent setStatus
+
+sleep 5
+Unregister-ScheduledTask -TaskName "Setup" -Confirm:$false
