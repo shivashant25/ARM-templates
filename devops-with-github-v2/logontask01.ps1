394e890ed187a7a74f9135b11b93f7cd018d957c
Shivashant <81952290+shivashant25@users.noreply.github.com>
2022-12-12 16:20:42 +0530

    Update logontask01.ps1

--- devops-with-github-v2/logontask01.ps1
+++ devops-with-github-v2/logontask01.ps1
@@ -176,6 +176,8 @@ az keyvault set-policy -n $KV_NAME --key-permissions get list --object-id  $AppI
 
 az keyvault set-policy -n $KV_NAME  --secret-permissions get list set --object-id $(az identity show --name "$AKS_CLUSTER_NAME-agentpool" -g $AKS_NODES_RESOURCE_GROUP_NAME --query "principalId" -o tsv)
 
+az keyvault set-policy -n $KV_NAME  --secret-permissions get list set --object-id  $AppID 
+
 az storage blob sync --account-name $STORAGE_ACCOUNT_NAME -c $PRODUCT_DETAILS_CONTAINER_NAME -s 'src/ContosoTraders.Api.Images/product-details'
 
 az storage blob sync --account-name $STORAGE_ACCOUNT_NAME -c $PRODUCT_LIST_CONTAINER_NAME -s 'src/ContosoTraders.Api.Images/product-list'
