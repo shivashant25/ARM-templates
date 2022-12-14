4a89b7ce9ad32c9348a2699cfb0ddf6141779291
Shivashant <81952290+shivashant25@users.noreply.github.com>
2022-12-14 18:14:01 +0530

    Update logontask01.ps1

--- devops-with-github-v2/logontask01.ps1
+++ devops-with-github-v2/logontask01.ps1
@@ -28,6 +28,21 @@ Install-Module Sqlserver -SkipPublisherCheck -Force
 Import-Module Sqlserver
 choco install kubernetes-cli
 
+#re-install bicep module if choco command fails
+# Create the install folder
+$installPath = "$env:USERPROFILE\.bicep"
+$installDir = New-Item -ItemType Directory -Path $installPath -Force
+$installDir.Attributes += 'Hidden'
+# Fetch the latest Bicep CLI binary
+(New-Object Net.WebClient).DownloadFile("https://github.com/Azure/bicep/releases/latest/download/bicep-win-x64.exe", "$installPath\bicep.exe")
+# Add bicep to your PATH
+$currentPath = (Get-Item -path "HKCU:\Environment" ).GetValue('Path', '', 'DoNotExpandEnvironmentNames')
+if (-not $currentPath.Contains("%USERPROFILE%\.bicep")) { setx PATH ($currentPath + ";%USERPROFILE%\.bicep") }
+if (-not $env:path.Contains($installPath)) { $env:path += ";$installPath" }
+# Verify you can now access the 'bicep' command.
+bicep --help
+# Done!
+
 . C:\LabFiles\AzureCreds.ps1
 
 $user = $AzureUserName
