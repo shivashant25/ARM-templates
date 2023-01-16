fa7fd7a4a14522a0faf9a1ab3530566ef4c1f0a6
Shivashant <81952290+shivashant25@users.noreply.github.com>
2023-01-16 11:24:57 +0530

    Create psscript01.ps1

---
+++ sdponboarding/psscript01.ps1
@@ -0,0 +1,99 @@
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
+    [string]
+    $AzurePassword,
+    [string]
+    $AzureTenantID,
+    [string]
+    $AzureSubscriptionID,
+    [string]
+    $ODLID,
+    [string]
+    $DeploymentID,
+    [string]
+    $azuserobjectid,
+    [string]
+    $InstallCloudLabsShadow,
+    [string]
+    $vmAdminUsername,
+    [string]
+    $trainerUserName,
+    [string]
+    $vmAdminPassword,
+    [string]
+    $trainerUserPassword
+
+)
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+$adminUsername = "demouser"
+[System.Environment]::SetEnvironmentVariable('DeploymentID', $DeploymentID,[System.EnvironmentVariableTarget]::Machine)
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+#Use the commonfunction to install the required files for cloudlabsagent service 
+CloudlabsManualAgent Install
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+WindowsServerCommon
+InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID $azuserobjectid
+InstallChocolatey
+
+sleep 10
+
+#Az Login
+
+. C:\LabFiles\AzureCreds.ps1
+
+$userName = $AzureUserName
+$password = $AzurePassword
+$subscriptionId = $AzureSubscriptionID
+$TenantID = $AzureTenantID
+
+
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+Connect-AzAccount -Credential $cred | Out-Null
+
+$Inputstring = $AzureUserName
+$CharArray =$InputString.Split("@")
+$CharArray[1]
+$tenantName = $CharArray[1]
+
+# Register SQLVM1 with 'Lightweight' SQL IaaS agent
+$RG-Name = SQL-EXTENSION-$DeploymentID
+$vm1 = Get-AzVM -Name SQLVM1 -ResourceGroupName $RG-Name
+
+New-AzSqlVM -Name $vm1.Name -ResourceGroupName $vm1.ResourceGroupName -Location $vm1.Location -LicenseType AHUB  -SqlManagementType LightWeight
+
+# Register SQLVM2 with 'Lightweight' SQL IaaS agent
+$RG-Name = SQL-EXTENSION-$DeploymentID
+$vm2 = Get-AzVM -Name SQLVM2 -ResourceGroupName $RG-Name
+
+New-AzSqlVM -Name $vm2.Name -ResourceGroupName $vm2.ResourceGroupName -Location $vm2.Location -LicenseType AHUB  -SqlManagementType LightWeight
+
+sleep 5
+
+Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
+
+#WSL 2 pacakage installation
+wsl --install
+(New-Object System.Net.WebClient).DownloadFile('https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi', 'C:\wsl_update_x64.msi')
+
+#Use the cloudlabs common function to write the status to validation status txt file 
+$Validstatus="Pending"  ##Failed or Successful at the last step
+$Validmessage="Post Deployment is Pending"
+CloudlabsManualAgent setStatus
+
+Stop-Transcript
+Restart-Computer -Force 
