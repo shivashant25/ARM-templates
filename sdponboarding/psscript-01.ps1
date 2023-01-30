5e9f7a02560df202b25dd60ed2e3dfd70848cb47
Shivashant <81952290+shivashant25@users.noreply.github.com>
2023-01-30 14:24:56 +0000

    added

---
+++ sdponboarding/psscript-01.ps1
@@ -0,0 +1,90 @@
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
+    [string]
+    $AzurePassword,
+    [string]
+    $AzureTenantID,
+    [string]
+    $AzureSubscriptionID,
+    [string]
+    $ODLID,
+    [string]
+    $DeploymentID,
+    [string]
+    $azuserobjectid,
+    [string]
+    $InstallCloudLabsShadow,
+    [string]
+    $vmAdminUsername,
+    [string]
+    $trainerUserName,
+    [string]
+    $vmAdminPassword,
+    [string]
+    $trainerUserPassword
+
+)
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+$adminUsername = "demouser"
+[System.Environment]::SetEnvironmentVariable('DeploymentID', $DeploymentID,[System.EnvironmentVariableTarget]::Machine)
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+WindowsServerCommon
+InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID $azuserobjectid
+InstallChocolatey
+
+sleep 10
+
+#install AZ-module latest version
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+Install-Module -Name Az -Force
+
+sleep 10
+
+#Az Login
+
+. C:\LabFiles\AzureCreds.ps1
+
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://experienceazure.blob.core.windows.net/templates/aiw-devops-with-github-v2/scripts/logontask-01.ps1","C:\Packages\logontask-01.ps1")
+
+
+$userName = $AzureUserName
+$password = $AzurePassword
+$subscriptionId = $AzureSubscriptionID
+$TenantID = $AzureTenantID
+
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+Connect-AzAccount -Credential $cred | Out-Null
+
+$Inputstring = $AzureUserName
+$CharArray =$InputString.Split("@")
+$CharArray[1]
+$tenantName = $CharArray[1]
+
+
+
+sleep 5
+
+Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
+
+
+Stop-Transcript
+Restart-Computer -Force 
