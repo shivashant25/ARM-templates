2c187785a459d45063846ada3791f68012777733
Shivashant <81952290+shivashant25@users.noreply.github.com>
2023-02-06 15:32:05 +0530

    Update psscript-01.ps1

--- sdponboarding/psscript-01.ps1
+++ sdponboarding/psscript-01.ps1
@@ -54,56 +54,6 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 
 Install-Module -Name Az -Force
 
-sleep 10
-
-#Az Login
-
-. C:\LabFiles\AzureCreds.ps1
-
-$userName = $AzureUserName
-$password = $AzurePassword
-$subscriptionId = $AzureSubscriptionID
-$TenantID = $AzureTenantID
-
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/shivashant25/ARM-templates/main/sdponboarding/deploy-02.json","C:\Packages\deploy-02.json")
-
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/shivashant25/ARM-templates/main/sdponboarding/deploy-02.parameters.json","C:\Packages\deploy-02.parameters.json")
-
-$path = "C:\Packages"
-(Get-Content -Path "$path\deploy-02.parameters.json") | ForEach-Object {$_ -Replace "deploymentidvalue", "$DeploymentID"} | Set-Content -Path "$path\deploy-02.parameters.json"
-
-sleep 5
-
-$path = "C:\Workspaces\lab\aiw-devops-with-github-lab-files\iac"
-(Get-Content -Path "$path\deploy-02.parameters.json") | ForEach-Object {$_ -Replace "azureusername", "$AzureUserName"} | Set-Content -Path "$path\deploy-02.parameters.json"
-
-sleep 5
-
-. C:\LabFiles\AzureCreds.ps1
-
-$userName = $AzureUserName
-$password = $AzurePassword
-$subscriptionId = $AzureSubscriptionID
-$TenantID = $AzureTenantID
-
-$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
-$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
-
-Connect-AzAccount -Credential $cred | Out-Null
-
-$Inputstring = $AzureUserName
-$CharArray =$InputString.Split("@")
-$CharArray[1]
-$tenantName = $CharArray[1]
-
-cd C:\Packages
-
-$RGname = "PowerBI-Embedded-RG"
-
-New-AzResourceGroupDeployment -Name "createresources" -TemplateFile "deploy-02.json" -TemplateParameterFile "deploy-02.parameters.json" -ResourceGroup $RGname
-
 sleep 5
 
 Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
