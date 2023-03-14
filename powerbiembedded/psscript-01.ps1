af7cb1a0926465bd79a30d96d9b57731104ef3bf
Shivashant <81952290+shivashant25@users.noreply.github.com>
2023-03-14 21:24:39 +0530

    Update psscript-01.ps1

--- powerbiembedded/psscript-01.ps1
+++ powerbiembedded/psscript-01.ps1
@@ -55,6 +55,41 @@ Install-Module -Name Az -Force
 
 sleep 5
 
+Install-Module -Name MicrosoftPowerBIMgmt -Force
+
+sleep 5
+
+#Download PBI reports
+
+cd C:\LabFiles
+
+(New-Object Net.WebClient).DownloadFile("https://github.com/CloudLabsAI-Azure/PowerBI-Embedded/blob/main/Lab_Guide/labfiles/Wingtip%20Sales%20Analysis.pbix", "C:\LabFiles\Wingtip Sales Analysis.pbix")
+
+(New-Object Net.WebClient).DownloadFile("https://github.com/CloudLabsAI-Azure/PowerBI-Embedded/blob/main/Lab_Guide/labfiles/Sales%20%26%20Returns%20Sample%20without%20RLS.pbix", "C:\LabFiles\Sales & Returns Sample without RLS.pbix")
+
+(New-Object Net.WebClient).DownloadFile("https://github.com/CloudLabsAI-Azure/PowerBI-Embedded/blob/main/Lab_Guide/labfiles/Sales%20%26%20Returns%20Sample%20with%20RLS.pbix", "C:\LabFiles\Sales & Returns Sample with RLS.pbix")
+
+#Az login
+. C:\LabFiles\AzureCreds.ps1
+
+$userName = $AzureUserName
+$password = $AzurePassword
+$subscriptionId = $AzureSubscriptionID
+$TenantID = $AzureTenantID
+
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+Connect-PowerBIServiceAccount -Credential $cred | Out-Null
+
+New-PowerBIWorkspace -Name "hacker$DeploymentID"
+
+cd C:\LabFiles
+
+$PBID = (Get-PowerBIWorkspace).Id
+
+New-PowerBIReport -Path 'C:\LabFiles\Wingtip Sales Analysis.pbix' -Name 'Wingtip Sales Analysis' -WorkspaceId $PBID
+
 Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
 
 Stop-Transcript
